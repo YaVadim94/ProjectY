4353d4228b9790f23cc518c8a1cc03d5db7d1a0a
VadimYarysh <yarish612@gmail.com>
2021-06-05 20:00:25 +0300

    Откатил докер-компос к дефолтному + создал контекст БД

---
+++ src/backend/ProjectY.Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectY.Data
+{
+    /// <summary>
+    /// Контекст базы данных
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
