9645771630eaba7a77affb24349a34a208743f9a
franticmax <frantic.max@gmail.com>
2021-06-06 14:57:05 +0300

    Добавил основную инфраструктуру

---
+++ src/backend/ProjectY.Web.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectY.Web.Api.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
