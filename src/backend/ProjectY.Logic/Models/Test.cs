8a98023b2810f407f451bdc90c721a9f0a76738a
franticmax <frantic.max@gmail.com>
2021-06-06 21:38:26 +0300

    Добавил модель для теста

---
+++ src/backend/ProjectY.Logic/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectY.Logic.Models
+{
+    /// <summary>
+    /// Тестовая модель.
+    /// </summary>
+    public class Test
+    {
+        public int Number { get; set; }
+    }
+}
