b4bee7aba3d0e1f13f89d630795bf5bd7151dad7
franticmax <frantic.max@gmail.com>
2021-06-28 21:13:38 +0300

    Добавил маппинг, контракты и контроллер

---
+++ src/ProjectY.Backend.Web.Api/Controllers/ShoesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ProjectY.Backend.Application.Logic.Interfaces;
+using ProjectY.Backend.Application.Logic.Models.Shoes;
+using ProjectY.Shared.Contracts.ShoesController;
+using ProjectY.Web.Api.Controllers;
+
+namespace ProjectY.Backend.Web.Api.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с обувью.
+    /// </summary>
+    public class ShoesController : BaseApiController
+    {
+        private readonly IShoesService _shoesService;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Конструктор контроллера для работы с обувью.
+        /// </summary>
+        /// <param name="shoesService">Сервис для работы с обувью</param>
+        /// <param name="mapper">Маппер</param>
+        public ShoesController(IShoesService shoesService, IMapper mapper)
+        {
+            _shoesService = shoesService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Создать обувь.
+        /// </summary>
+        [HttpPost]
+        public async Task<ShoesContracts> CreateShoes(CreateShoesContract request)
+        {
+            var createdShoesDto = _mapper.Map<CreateShoesDto>(request);
+
+            var result = await _shoesService.CreateAsync(createdShoesDto);
+
+            return _mapper.Map<ShoesContracts>(result);
+        }
+
+        /// <summary>
+        /// Получить обувь по идентификатору.
+        /// </summary>
+        [HttpGet]
+        public async Task<ShoesContracts> GetShoesById(long id)
+        {
+            var result = await _shoesService.GetByIdAsync(id);
+
+            return _mapper.Map<ShoesContracts>(result);
+        }
+    }
+}
---
+++ src/ProjectY.Backend.Web.Api/Profiles/ShoesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ProjectY.Backend.Application.Logic.Models.Shoes;
+using ProjectY.Shared.Contracts.ShoesController;
+
+namespace ProjectY.Backend.Web.Api.Profiles
+{
+    /// <summary>
+    /// Класс для конфигурации маппинга.
+    /// </summary>
+    public class ShoesProfile : Profile
+    {
+        /// <summary>
+        /// Конструктор класса для конфигурации маппинга.
+        /// </summary>
+        public ShoesProfile()
+        {
+            CreateMap<CreateShoesContract, CreateShoesDto>();
+            CreateMap<ShoesDto, ShoesContracts>();
+        }
+    }
+}
