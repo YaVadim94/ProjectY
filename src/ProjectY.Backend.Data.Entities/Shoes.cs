8e9e6319ccf11de347b4479e16f87256fc2560d7
franticmax <frantic.max@gmail.com>
2021-06-28 20:47:40 +0300

    Добавил сущность для обуви

---
+++ src/ProjectY.Backend.Data.Entities/Shoes.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjectY.Backend.Data.Entities
+{
+    /// <summary>
+    /// Сущность обувных моделей.
+    /// </summary>
+    public class Shoes : BaseEntity
+    {
+        /// <summary>
+        /// Наименование модели.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Артикул модели.
+        /// </summary>
+        public int VendorCode { get; set; }
+
+        /// <summary>
+        /// Тип модели.
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// Цвет модели.
+        /// </summary>
+        public int Color { get; set; }
+
+        // TODO: дозаполнить остальными свойствами
+        // TODO: реализовать проперти: "доступные размеры" (как навигационное свойство?)
+    }
+}
